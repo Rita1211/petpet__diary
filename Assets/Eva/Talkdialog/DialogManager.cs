e098583fb338e6e2f91fc9ebd551e9b5169aae81
tkuimCKY409631248 <138713096+tkuimCKY409631248@users.noreply.github.com>
2023-11-23 14:09:47 +0800

    1122test

---
+++ Assets/Eva/Talkdialog/DialogManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// 對話文本文件 csv格式
+    /// </summary>
+    public TextAsset dialogDataFile;
+    public TMP_Text nameText;
+    public TMP_Text dialogText;
+
+    public int dialogIndex;
+    public Button nextBTN;
+    public GameObject optionBTN;//選項按鈕欲置物
+    public GameObject ButtonGroup;//選像按鈕父節點用於自動排列
+
+    /// <summary>
+    /// 對話文本 依行數分割
+    /// </summary>
+    public string[] dialogRows;
+
+    void Start()
+    {
+        ReadText(dialogDataFile);
+        ShowDialogRow();
+        //UpdateText("獸醫","");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateText(string _name,string _text)
+    {
+        nameText.text=_name;
+        dialogText.text=_text;
+    }
+    public void ReadText(TextAsset _textAsset)
+    {
+        dialogRows = _textAsset.text.Split('\n');
+        //foreach(var row in rows)
+        //{
+        //    string[] cell = row.Split(',');
+        //}
+        Debug.Log("讀取成功");
+    }
+    public void ShowDialogRow()
+    {
+        for( int i=0;i<dialogRows.Length;i++)
+        {
+           string[] cells =dialogRows[i].Split(','); 
+           if(cells[0]=="#" && int.Parse(cells[1]) == dialogIndex)
+           {
+                UpdateText(cells[2],cells[3]);
+                dialogIndex=int.Parse(cells[4]);
+                break;
+           }
+           else if (cells[0]=="&" && int.Parse(cells[1]) == dialogIndex)
+           {
+                nextBTN.gameObject.SetActive(false);
+                GenerateOption(i);
+           }
+        }
+        
+    }
+    public void onClicknextBtn()
+    {
+        ShowDialogRow();
+    }
+    public void GenerateOption(int _index)
+    {
+        string [] cells = dialogRows[_index].Split(',');
+        if(cells[0]=="&")
+        {
+            string[] cells = dialogRows[_index].Split(',');
+            if(cells[0]=="&")
+            {
+                GameObject button =Instantiate(optionBTN,ButtonGroup);
+                //綁定按鈕事件
+                button.GetComponentInChildren<TextMeshPro>().text=cells[3];
+                button.GetComponent<ButtonGroup>().onClick.AddListener();
+                GenerateOption(_index+1);
+            }                        
+        }        
+    }
+    public void OnOptionClick(int _id)
+    {
+        
+    }
+}
